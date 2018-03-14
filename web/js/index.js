5c5292c33348386d60070cd840cdc6e75cf63c9a
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2018-03-13 19:07:24 -0600

    ewe

--- web/js/index.js
+++ web/js/index.js
@@ -16,4 +16,20 @@ $('button').on('click', function() {
 
 
 
+var card12 = document.querySelector(".card--12");
 
+card12.addEventListener('mousemove', function (e) {
+
+  var wh = window.innerHeight / 2,
+  ww = window.innerWidth / 2;
+  card12.style.setProperty('--mouseX12', (e.clientX - ww) / 25);
+  card12.style.setProperty('--mouseY12', (e.clientY - wh) / 25);
+
+});
+
+card12.addEventListener('mouseleave', function (e) {
+
+  card12.style.setProperty('--mouseX12', 0);
+  card12.style.setProperty('--mouseY12', 0);
+
+});
