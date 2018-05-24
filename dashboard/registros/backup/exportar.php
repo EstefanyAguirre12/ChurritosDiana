a22106c3ff55225ec4e8e7983800b6454ebde9c9
Danicx29 <daniel.alex.barrera@hotmail.com>
2018-05-24 12:37:23 -0600

    hola

--- dashboard/registros/backup/exportar.php
+++ dashboard/registros/backup/exportar.php
@@ -1,5 +1,5 @@
 <?php
-/*Comentario de prueba x4*/
+/*Comentario de prueba x5*/
 require_once("../../../app/views/dashboard/panel/panel.class.php");
 Page::templateHeader("empleados");
 require_once("../../../app/controllers/dashboard/registros/backup/exportar_controller.php");
