80395130646c3ffe2453518b16b2ce37bc3ae8b0
Andresss21 <estefanyaguirre1200@hotmail.com>
2018-07-17 16:19:59 -0600

    d

---
+++ dashboard/graficos/tipoente.php
@@ -0,0 +1,40 @@
+<?php
+include_once('php/tipoentes.php');
+/*include_once('php/usuarios.php');*/
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<script>
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Cantidad de cliente por rol"
+	},
+	data: [{
+    type: "splineArea",
+		color: "rgba(54,158,173,.7)",
+		markerSize: 5,
+		xValueFormatString: "YYYY",
+		yValueFormatString: "$#,##0.##",
+
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+
+
+}
+</script>
+
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
---
+++ dashboard/graficos/tipoproducto.php
@@ -0,0 +1,43 @@
+<?php
+include_once('php/tipoproducto.php');
+/*include_once('php/usuarios.php');*/
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<script>
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Cantidad de productos por categoria"
+	},
+	data: [{
+    type: "pyramid",
+		indexLabel: "{label} - {y}",
+    showInLegend: true,
+    legendText: "{label}",
+		toolTipContent: "Cantidad de productos en <b>{label}</b>: {y}",
+		neckWidth: 20,
+		neckHeight: 0,
+		valueRepresents: "area",
+
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+
+
+}
+</script>
+
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
