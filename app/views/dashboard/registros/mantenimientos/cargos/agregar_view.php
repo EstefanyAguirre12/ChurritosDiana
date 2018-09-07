a986e86115543dc54f122f048e0a081b28925d10
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2018-09-07 09:06:33 -0600

    hh

--- app/views/dashboard/registros/mantenimientos/cargos/agregar_view.php
+++ app/views/dashboard/registros/mantenimientos/cargos/agregar_view.php
@@ -34,6 +34,34 @@
                         <input id="middle-name  " name="des" class="form-control col-md-7 col-xs-12 validate" type="text" name="middle-name" value="<?php print($dato->getDescripcion()) ?>" required/>
                       </div>
                     </div>
+
+
+                    <div class="form-group">
+                      <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Descripcion*:</label>
+                      <div class="col-md-6 col-sm-6 col-xs-12">
+                      <input type="checkbox" id="permSolicitudes" name="permSolicitudes" />
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Descripcion*:</label>
+                      <div class="col-md-6 col-sm-6 col-xs-12">
+                      <input type="checkbox" id="permSolicitudes" name="permSolicitudes" />
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Descripcion*:</label>
+                      <div class="col-md-6 col-sm-6 col-xs-12">
+                      <input type="checkbox" id="permSolicitudes" name="permSolicitudes" />
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Descripcion*:</label>
+                      <div class="col-md-6 col-sm-6 col-xs-12">
+                      <input type="checkbox" id="permSolicitudes" name="permSolicitudes" />
+                      </div>
+                    </div>
+                    
+                    
                     <div class="ln_solid"></div>
                     <div class="form-group">
                       <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
