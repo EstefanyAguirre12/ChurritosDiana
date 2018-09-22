ac7efef2212ce5a5fa6df1ab214f2e9fd28fc087
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2018-09-22 14:41:36 -0600

    el 4

--- app/views/dashboard/panel/panel.class.php
+++ app/views/dashboard/panel/panel.class.php
@@ -394,7 +394,7 @@ class Page extends Component{
 					<script src='https://www.google.com/recaptcha/api.js'></script>
 					<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
 					<link type='text/css' rel='stylesheet' href='../../../web/css/custom.css'/>
-					<script type='text/javascript' src='../../../web/js/sweetalert.min.js'></script>4
+					<script type='text/javascript' src='../../../web/js/sweetalert.min.js'></script>
 					<meta name='viewport' content='width=device-width, initial-scale=1.0'/>
 				</head>
 				<body class='nav-md '>
